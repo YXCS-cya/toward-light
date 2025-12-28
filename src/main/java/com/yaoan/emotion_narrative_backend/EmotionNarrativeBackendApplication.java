fe5886cdc95a94ba8a2155522f94d3834a11d501
YXCS-cya <162129534+YXCS-cya@users.noreply.github.com>
2025-12-28 23:16:04 +0800

    init spring boot backend project

---
+++ src/main/java/com/yaoan/emotion_narrative_backend/EmotionNarrativeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yaoan.emotion_narrative_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotionNarrativeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmotionNarrativeBackendApplication.class, args);
+	}
+
+}
